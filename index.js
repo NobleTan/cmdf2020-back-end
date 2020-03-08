78e5a6086e4c2a6ee14310927c8f49bbc54e51d9
NobleTan <noblet@sfu.ca>
2020-03-07 19:36:50 -0800

    database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
+const {Client} = require("pg");
+const client = new Client();
 app.get("/", (req, res) => {
-    res.send("hello world")
+    client.query("SELECT * FROM account", (err, dbres) => {
+        res.send(dbres.row[0])        
+    })
 })
 
 app.listen(PORT,()=>{});
