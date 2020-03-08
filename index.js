17e10c9d1ca42a9fab1afba2d12419576ee61d8f
Shirley Bi <shirley.bi@sap.com>
2020-03-08 01:57:19 -0800

    transfer server endpoints

--- index.js
+++ index.js
@@ -11,13 +11,45 @@ const client = new Client({
     ssl: true
 });
 
+var indexRouter = require('./routes/index');
+var speechRouter = require('./routes/speech');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/speech', speechRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 app.get("/", (req, res) => {
     console.log("hello")
     client.connect().then(()=> {
         client.query("SELECT * FROM account;", (err, dbres) => {
             console.log(err)
             console.log(dbres)
-            res.json(dbres.rows[0])        
+            res.json(dbres.rows[0])
         })
     })
 })
@@ -52,13 +84,13 @@ app.post('/reg', (req, res) => {        // loads new reg to database +check if u
       res.render("pages/home", { props: { 'login': true } });
     });
   })
-  
+
   app.get("/home", (req, res) => {
     res.render("pages/home", { "props": { loginFailed: false } })
   })
-  
-  
-  
+
+
+
   app.post("/pages/login", (req, res) => {
     //console.log(req.body);
       var queryString = `SELECT * FROM account WHERE username='${req.body.username}';`;
@@ -66,13 +98,48 @@ app.post('/reg', (req, res) => {        // loads new reg to database +check if u
           if(error)
               res.send(error);
           if(result.rows.length > 0 && result.rows[0].password === req.body.password){
-    
+
             res.render("pages/play", {'props': {username: req.body.username}});    // load user page for users
             return;
-              
+
           }
           res.render('pages/home', {'props': {loginFailed: true}});
       })
-  
+
   })
-  
+
+var indexRouter = require('./routes/index');
+var speechRouter = require('./routes/speech');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/speech', speechRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
