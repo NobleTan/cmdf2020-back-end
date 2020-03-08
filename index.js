d5e4337e0ec554e98084b4f15abaef78ab1dd31e
NobleTan <noblet@sfu.ca>
2020-03-08 01:40:26 -0800

    reg/login

--- index.js
+++ index.js
@@ -23,3 +23,56 @@ app.get("/", (req, res) => {
 })
 
 app.listen(PORT,()=>{});
+
+
+app.post('/reg', (req, res) => {        // loads new reg to database +check if username already exist
+    console.log(req.body);
+    let body = req.body;
+    let userCheck = `SELECT * FROM account WHERE username = '${body.username}';`;
+    pool.query(userCheck, (error, result) => {
+      if (result.rows.length > 0) {
+        res.render('pages/home', { 'props': { regFailed: true } });
+        return;
+      }
+      var getUsersQuery = `INSERT INTO users (username , password) VALUES ('${body.username}' , '${body.password}');`;
+      console.log(getUsersQuery);
+      pool.query(getUsersQuery, (error, result) => {
+        if (error) {
+          res.send("error");
+          console.log(error);
+        }
+      });
+      console.log("passed User Query\n");
+      pool.query(getStatsQuery, (error, result) => {
+        if (error) {
+          res.send("error");
+          console.log(error);
+        }
+      });
+      res.render("pages/home", { props: { 'login': true } });
+    });
+  })
+  
+  app.get("/home", (req, res) => {
+    res.render("pages/home", { "props": { loginFailed: false } })
+  })
+  
+  
+  
+  app.post("/pages/login", (req, res) => {
+    //console.log(req.body);
+      var queryString = `SELECT * FROM account WHERE username='${req.body.username}';`;
+      pool.query(queryString, (error, result) => {
+          if(error)
+              res.send(error);
+          if(result.rows.length > 0 && result.rows[0].password === req.body.password){
+    
+            res.render("pages/play", {'props': {username: req.body.username}});    // load user page for users
+            return;
+              
+          }
+          res.render('pages/home', {'props': {loginFailed: true}});
+      })
+  
+  })
+  
